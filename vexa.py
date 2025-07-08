10e0ffe738cf2e0b2e6e1c30f1989ef9eb3c9ac3
Noll <noll46658@gmail.com>
2025-07-08 15:11:01 -0700

    Add files via upload

---
+++ vexa.py
@@ -0,0 +1,76 @@
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import wikipedia
+import webbrowser
+import os
+import pyjokes
+
+def speak(text):
+    print(f"Vexa: {text}")
+    try:
+        engine = pyttsx3.init()
+        engine.say(text)
+        engine.runAndWait()
+    except:
+        print("Speech output not supported in your IDE.")
+def wish_user():
+    hour = int(datetime.datetime.now().hour)
+    if hour < 12:
+        speak("Good Morning!")
+    elif hour < 18:
+        speak("Good Afternoon!")
+    else:
+        speak("Good Evening!")
+    speak("I am your voice assistant, Vexa. How can I help you today?")
+def take_command():
+    return input("You (type your command): ").lower()
+def run_assistant():
+    wish_user()
+    while True:
+        query = take_command()
+        if 'wikipedia' in query:
+            speak("Searching Wikipedia...")
+            query = query.replace("wikipedia", "")
+            try:
+                result = wikipedia.summary(query, sentences=2)
+                speak("According to Wikipedia:")
+                speak(result)
+            except:
+                speak("Sorry, I couldn't find anything.")
+
+        elif 'open youtube' in query:
+            speak("Opening YouTube...")
+            webbrowser.open("https://www.youtube.com")
+
+        elif 'open brave' in query:
+            speak("Opening Brave...")
+            webbrowser.open("https://search.brave.com/")
+
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"The current time is {strTime}")
+
+        elif 'joke' in query:
+            joke = pyjokes.get_joke()
+            speak(joke)
+
+        elif 'exit' in query or 'bye' in query:
+            speak("Goodbye! Have a nice day!")
+            break
+        elif 'hello' in query:
+            speak("Hello. How can I help you today?")
+        elif 'browser' in query:
+            speak("Brave and Tor are great for privacy.")
+        elif 'python' in query:
+            speak("The best language.")
+        elif 'scratch' in query:
+            speak("The worst language.")
+        elif 'Alexa' in query:
+            speak("Hate it.")
+
+        elif 'chatgpt' in query:
+            speak("I hate chatgpt.")
+        else:
+            speak("Sorry, I didn't understand that. Try again.")
+run_assistant()
